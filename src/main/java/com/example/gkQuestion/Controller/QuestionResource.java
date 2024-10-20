e9c5a5d9b023dde1252e192ac8e11e1f7837a5fd
Tribhuvan nath <tribhuvannath4567@gmail.com>
2024-10-20 16:22:42 +0530

    Demo Code

--- src/main/java/com/example/gkQuestion/Controller/QuestionResource.java
+++ src/main/java/com/example/gkQuestion/Controller/QuestionResource.java
@@ -8,45 +8,58 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class QuestionResource {
-    private static List<Question> list = new ArrayList<>();
-    private static int count = 0;
+        private static List<Question> list = new ArrayList<>();
+        private static int count = 0;
 
-    static {
-        list.add(new Question(++count, "Who is the chief minister of bihar?", "Shree nitish kumar"));
-        list.add(new Question(++count,
-                "Grand Central Terminal, Park Avenue, New York is the world's", "largest railway station"));
-        list.add(new Question(++count, "Who was the first woman to win a Nobel Prize?",
-                "Marie Curie, for Physics in 1903."));
-        list.add(new Question(++count, "Who painted the Mona Lisa?", " Leonardo da Vinci."));
-        list.add(new Question(++count, "Which country is known as the Land of the Rising Sun?", "Japan"));
-        list.add(new Question(++count, "What is the chemical symbol for iron?", "Fe"));
-        list.add(new Question(++count, "Who is known as the father of modern physics? ", "Albert Einstein."));
-        list.add(new Question(++count, "What is the chemical symbol for the element gold?", "Au."));
-        list.add(new Question(++count, "Who wrote the play “Romeo and Juliet”? ", " William Shakespeare."));
-        list.add(new Question(++count, "What is the capital city of Australia? ", " Canberra."));
-        list.add(new Question(++count, "Which planet is known as the Red Planet? ", "Mars."));
-        list.add(new Question(++count, "What is the longest river in the United States?", "The Missouri River."));
-        list.add(new Question(++count, "What is the highest mountain peak in India?", "Kangchenjunga."));
-        list.add(new Question(++count, "Which river is the longest in India?", "The Ganges River."));
-        list.add(new Question(++count, "Which beach holds the title for being the longest in India?",
-                "Marina Beach in Chennai, Tamil Nadu."));
-        list.add(new Question(++count, "What is the highest civilian award given in India?", "Bharat Ratna."));
-        list.add(new Question(++count, "Which lake is the largest in terms",
-                "Wular Lake in Jammu and Kashmir."));
-        list.add(new Question(++count, "What is the name of the highest waterfall in India?", //
-                "Jog Falls, located in Karnataka."));
-        list.add(new Question(++count, "Which national park is the largest in India?",
-                "Hemis National Park in Ladakh."));
-        list.add(new Question(++count, "Which mountain peak is the highest in the world?",
-                "Mount Everest located in the Himalayas."));
-    }
+        static {
+                list.add(new Question(++count, "Who is the chief minister of bihar?", "Shree nitish kumar"));
+                list.add(new Question(++count,
+                                "Grand Central Terminal, Park Avenue, New York is the world's",
+                                "largest railway station"));
+                list.add(new Question(++count, "Who was the first woman to win a Nobel Prize?",
+                                "Marie Curie, for Physics in 1903."));
+                list.add(new Question(++count, "Who painted the Mona Lisa?", " Leonardo da Vinci."));
+                list.add(new Question(++count, "Which country is known as the Land of the Rising Sun?", "Japan"));
+                list.add(new Question(++count, "What is the chemical symbol for iron?", "Fe"));
+                list.add(new Question(++count, "Who is known as the father of modern physics? ", "Albert Einstein."));
+                list.add(new Question(++count, "What is the chemical symbol for the element gold?", "Au."));
+                list.add(new Question(++count, "Who wrote the play “Romeo and Juliet”? ", " William Shakespeare."));
+                list.add(new Question(++count, "What is the capital city of Australia? ", " Canberra."));
+                list.add(new Question(++count, "Which planet is known as the Red Planet? ", "Mars."));
+                list.add(new Question(++count, "What is the longest river in the United States?",
+                                "The Missouri River."));
+                list.add(new Question(++count, "What is the highest mountain peak in India?", "Kangchenjunga."));
+                list.add(new Question(++count, "Which river is the longest in India?", "The Ganges River."));
+                list.add(new Question(++count, "Which beach holds the title for being the longest in India?",
+                                "Marina Beach in Chennai, Tamil Nadu."));
+                list.add(new Question(++count, "What is the highest civilian award given in India?", "Bharat Ratna."));
+                list.add(new Question(++count, "Which lake is the largest in terms",
+                                "Wular Lake in Jammu and Kashmir."));
+                list.add(new Question(++count, "What is the name of the highest waterfall in India?", //
+                                "Jog Falls, located in Karnataka."));
+                list.add(new Question(++count, "Which national park is the largest in India?",
+                                "Hemis National Park in Ladakh."));
+                list.add(new Question(++count, "Which mountain peak is the highest in the world?",
+                                "Mount Everest located in the Himalayas."));
+        }
 
-    public List<Question> findAll() {
-        return list;
-    }
+        public List<Question> findAll() {
+                return list;
+        }
 
-    public Question findOne(int id) {
-        Predicate<? super Question> predicate = que -> que.getId().equals(id);
-        return list.stream().filter(predicate).findFirst().orElse(null);
-    }
+        public Question findOne(int id) {
+                Predicate<? super Question> predicate = que -> que.getId().equals(id);
+                return list.stream().filter(predicate).findFirst().orElse(null);
+        }
+
+        public void deleteById(int id) {
+                Predicate<? super Question> predicate = que -> que.getId().equals(id);
+                list.removeIf(predicate);
+        }
+
+        public Question savQuestion(Question question) {
+                question.setId(++count);
+                list.add(question);
+                return question;
+        }
 }
--- src/main/java/com/example/gkQuestion/Controller/QuestionService.java
+++ src/main/java/com/example/gkQuestion/Controller/QuestionService.java
@@ -1,10 +1,16 @@
 package com.example.gkQuestion.Controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.gkQuestion.Exception.QuestionNotFoundException;
 
@@ -31,5 +37,18 @@ public class QuestionService {
         }
 
         return question;
+    }
+
+    @DeleteMapping("/question/{id}")
+    public void deleteAQuestino(@PathVariable int id) {
+        resource.deleteById(id);
+    }
+
+    @PostMapping("/question")
+    public ResponseEntity<Question> createQuestion(@RequestBody Question question) {
+        Question saveQuestion = resource.savQuestion(question);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(saveQuestion.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
